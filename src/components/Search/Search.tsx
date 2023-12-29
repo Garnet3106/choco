9eda8f5ae6cf3bbb9bddae5f9154edd8616c7820
Garnet3106 <private@garnet.works>
2023-12-29 18:55:01 +0900

    Fix styles

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,10 +3,28 @@ import './Search.css';
 export default function Search() {
   return (
     <div className='search'>
-      <input type='text' className='search-bar' />
-      <div className='search-results'>
+      <input type='text' className='search-bar' placeholder='Enter keyword' />
+      <div className='search-results scrollbar-none'>
         <div className='search-result-item'>
           “Google検索” で検索する
+        </div>
+        <div className='search-result-item'>
+          <span>JavaScript map関数 - <b>Google</b> 検索</span>
+          <span className='search-result-item-caption'>
+            google.com
+          </span>
+        </div>
+        <div className='search-result-item'>
+          <span>JavaScript map関数 - <b>Google</b> 検索</span>
+          <span className='search-result-item-caption'>
+            google.com
+          </span>
+        </div>
+        <div className='search-result-item'>
+          <span>JavaScript map関数 - <b>Google</b> 検索</span>
+          <span className='search-result-item-caption'>
+            google.com
+          </span>
         </div>
         <div className='search-result-item'>
           <span>JavaScript map関数 - <b>Google</b> 検索</span>
