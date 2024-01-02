2000757927c307dad31e91b1542e988c5e0bdc2c
Garnet3106 <private@garnet.works>
2024-01-02 14:21:05 +0900

    Add React Router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Search from '../Search/Search';
+import Settings from '../Settings/Settings';
 
 export default function App() {
   return (
     <main className='app'>
-      <Search />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={Search} />
+          <Route path='/settings' Component={Settings} />
+          <Route path='/index.html' element={<Navigate to='/' />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
